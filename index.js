d2d71f73c5978723555bd6bf76bca28f32890297
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-12-11 01:38:48 -0500

    added file watch and server restart

--- index.js
+++ index.js
@@ -11,29 +11,62 @@ var pkg = require(path.join(__dirname, 'package.json'));
 var configFilename = 'turtles.json';
 console.log('looking for config file', configFilename);
 
-var config = JSON.parse(fs.readFileSync(configFilename), 'utf-8');
+if (!fs.existsSync(configFilename)) {
+  console.error('Cannot find config file', configFilename);
+  process.exit(-1);
+}
 
-var slowDowns = Object.keys(config.urls).map(function (urlPattern) {
-  var delay = config.urls[urlPattern];
-  return slow({
-    url: new RegExp(urlPattern),
-    delay: delay
+var proxy, server;
+
+function startProxy() {
+  proxy = httpProxy.createProxyServer();
+
+  var config = JSON.parse(fs.readFileSync(configFilename), 'utf-8');
+
+  var slowDowns = Object.keys(config.urls).map(function (urlPattern) {
+    var delay = config.urls[urlPattern];
+    return slow({
+      url: new RegExp(urlPattern),
+      delay: delay
+    });
   });
+
+  server = http.createServer(function (req, res) {
+    var toTarget = proxy.web.bind(proxy, req, res, {
+      target: config.target
+    });
+
+    var slowPyramid = slowDowns.reduce(function (prev, slowDown) {
+      return slowDown.bind(null, req, res, prev);
+    }, toTarget);
+
+    slowPyramid();
+
+  }).listen(config.port);
+
+  console.log(pkg.name, 'listening at port', config.port);
+}
+
+var nodemon = require('nodemon');
+
+nodemon({
+  script: configFilename,
+  ext: 'json'
 });
 
-var proxy = httpProxy.createProxyServer();
+nodemon.on('start', function () {
+  if (server) {
+    server.close();
+  }
+  if (proxy) {
+    proxy.close();
+  }
+  startProxy();
+}).on('quit', function () {
+  server.close();
+  proxy.close();
+}).on('restart', function (files) {
+  console.log('restarting', pkg.name, 'on', configFilename, 'change');
+});
 
-http.createServer(function (req, res) {
-  var toTarget = proxy.web.bind(proxy, req, res, {
-    target: config.target
-  });
-
-  var slowPyramid = slowDowns.reduce(function (prev, slowDown) {
-    return slowDown.bind(null, req, res, prev);
-  }, toTarget);
-
-  slowPyramid();
-
-}).listen(config.port);
-
-console.log(pkg.name, 'listening at port', config.port);
+console.log('you can edit file', configFilename, 'to restart with new settings');
